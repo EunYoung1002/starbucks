e5bc7d0ae92d9e5f6b46b027c443ca5833e77941
EunYoung1002 <dhdmsdud1002@gmail.com>
2023-08-21 15:24:31 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-// header
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function(){
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function(){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-searchInputEl.addEventListener('blur', function(){
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -151,5 +134,3 @@ function random(min, max) {
   });
 
 
-  const thisYear = document.querySelector('.this-year');
-  thisYear.textContent = new Date().getFullYear();
